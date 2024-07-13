c86aff27c882ff997c2b91c6c143016c584d52a8
giff <gifriend23@gmail.com>
2024-07-13 17:44:27 +0800

    add song_book

---
+++ lib/features/song_book/presentations/song_screen.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:palakat/core/widgets/scaffold/scaffold_widget.dart';
+import 'package:palakat/core/widgets/screen_title/screen_title_widget.dart';
+
+class SongScreen extends StatelessWidget {
+  const SongScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ScaffoldWidget(
+      child: Column(
+        children: [const ScreenTitleWidget.titleOnly(title: 'Kidung Jemaat')],
+      ),
+    );
+  }
+}
