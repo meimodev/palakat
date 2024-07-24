26ad56bb028efe6cc8fb400ddb1a7ca98791e79e
giff <gifriend23@gmail.com>
2024-07-24 11:54:07 +0800

    merge verse and song data and refactor

--- lib/features/song_detail/presentations/song_details_screen.dart
+++ lib/features/song_detail/presentations/song_details_screen.dart
@@ -1,21 +1,21 @@
-import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:palakat/core/assets/assets.dart';
 import 'package:palakat/core/constants/constants.dart';
 import 'package:palakat/core/utils/extensions/extension.dart';
-import 'package:palakat/core/widgets/scaffold/scaffold_widget.dart';
-import 'package:palakat/core/widgets/screen_title/screen_title_widget.dart';
-import 'package:palakat/features/song_detail/presentations/song_details_data.dart';
+import 'package:palakat/core/widgets/widgets.dart';
+import 'package:palakat/features/presentation.dart';
 
 class SongDetails extends StatelessWidget {
   SongDetails({super.key});
 
   final DummyData dummyData = DummyData();
+  final SongPart songPart = SongPart();
 
   @override
   Widget build(BuildContext context) {
     return ScaffoldWidget(
       child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
         children: [
           ScreenTitleWidget.primary(
             title: dummyData.titles,
@@ -27,70 +27,57 @@ class SongDetails extends StatelessWidget {
             },
           ),
           Gap.h24,
-          Align(
-            alignment: Alignment.centerLeft,
-            child: Text(
-              dummyData.verse[0],
-              style: BaseTypography.bodyMedium.toBold.toSecondary,
-            ),
+          Text(
+            songPart.data[0],
+            style: BaseTypography.bodyMedium.toBold.toSecondary,
           ),
           Gap.h6,
           Text(
-            dummyData.data[0],
+            songPart.data[5],
             style: BaseTypography.bodyMedium.toPrimary,
           ),
           Gap.h12,
-          Align(
-            alignment: Alignment.centerLeft,
-            child: Text(
-              dummyData.verse[1],
-              style: BaseTypography.bodyMedium.toBold.toSecondary,
-            ),
+          Text(
+            songPart.data[1],
+            style: BaseTypography.bodyMedium.toBold.toSecondary,
           ),
           Gap.h6,
           Text(
-            dummyData.data[1],
+            songPart.data[7],
             style: BaseTypography.bodyMedium.toPrimary,
           ),
           Gap.h12,
-          Align(
-            alignment: Alignment.centerLeft,
-            child: Text(
-              dummyData.verse[2],
-              style: BaseTypography.bodyMedium.toBold.toSecondary,
-            ),
+          Text(
+            songPart.data[2],
+            style: BaseTypography.bodyMedium.toBold.toSecondary,
           ),
           Gap.h12,
-          Align(
-            alignment: Alignment.centerLeft,
-            child: Text(
-              dummyData.verse[3],
-              style: BaseTypography.bodyMedium.toBold.toSecondary,
-            ),
+          Text(
+            songPart.data[3],
+            style: BaseTypography.bodyMedium.toBold.toSecondary,
           ),
           Gap.h12,
-          Align(
-            alignment: Alignment.centerLeft,
-            child: Text(
-              dummyData.verse[4],
-              style: BaseTypography.bodyMedium.toBold.toSecondary,
-            ),
+          Text(
+            textAlign: TextAlign.left,
+            songPart.data[4],
+            style: BaseTypography.bodyMedium.toBold.toSecondary,
           ),
           Gap.h6,
           Text(
-            dummyData.data[2],
+            songPart.data[6],
             style: BaseTypography.bodyMedium.toPrimary,
           ),
           Gap.h24,
-          Align(
-            alignment: Alignment.centerLeft,
-            child: Text(
-              dummyData.youtube,
-              style: BaseTypography.bodyMedium.toBold.toSecondary,
-            ),
+          Text(
+            dummyData.youtube,
+            style: BaseTypography.bodyMedium.toBold.toSecondary,
           ),
           Gap.h6,
-          CachedNetworkImage(imageUrl: dummyData.image)
+          ImageNetworkWidget(
+            imageUrl: dummyData.image,
+            height: 336.0,
+            width: 736.0,
+          )
         ],
       ),
     );
